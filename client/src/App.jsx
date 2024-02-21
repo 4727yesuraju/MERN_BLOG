01dd78791aa5d44b160b967f61f5d65b48b1967d
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 <4727yesuraju@gmail.com>
2024-02-21 15:19:38 +0530

    create pages and route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { Home } from "./pages/Home"
+import { About } from "./pages/About"
+import { SignUp } from "./pages/SignUp"
+import { SignIn } from "./pages/SignIn"
+import { Dashboard } from "./pages/Dashboard"
+import { Projects } from "./pages/Projects"
+
+
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      app
-    </h1>
+    <BrowserRouter>
+       <Routes>
+         <Route path="/"  element={<Home />}/>
+         <Route path="/about"  element={<About />}/>
+         <Route path="/sign-in"  element={<SignIn />}/>
+         <Route path="/sign-up"  element={<SignUp />}/>
+         <Route path="/dashboard"  element={<Dashboard />}/>
+         <Route path="/projects"  element={<Projects />}/>
+         <Route path='/*' element={<h1>invalid route parameter</h1>} />
+       </Routes>
+    </BrowserRouter>
   )
 }
 
